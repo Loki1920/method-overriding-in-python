45b009e7887b4934705caa60d1b5fe87baada9f3
LUCKY TIWARI <89370329+Loki1920@users.noreply.github.com>
2022-03-28 19:06:24 +0530

    Add files via upload

---
+++ main (5).py
@@ -0,0 +1,12 @@
+#method overriding in python
+class Add:
+  def result(self,a,b):
+    print('Addition',a+b)
+
+class Multi:
+  def result(self,a,b):
+    super().result(10, 20)
+    print('Multiplication',a*b)
+
+m = Multi()
+m.result(10,20)
